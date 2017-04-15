fa97c6caf049f9afa0c20d74e02b8f938bf11ec5
Subhrajyoti123 <subhra.brahma9@gmail.com>
2017-04-15 23:26:06 +0530

    WebService
    Assignment14.2

---
+++ app/src/main/java/com/example/somina/myapp14/DataHandler.java
@@ -0,0 +1,32 @@
+package com.example.somina.myapp14;
+
+
+class DataHandler {
+
+    private int id, vote_count;
+    private String name;
+
+    int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    int getVote_count() {
+        return vote_count;
+    }
+
+    void setVote_count(int vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/network/com/example/somina/myapp14/network/OnWebServiceResult.java
@@ -0,0 +1,7 @@
+package com.example.somina.myapp14.network;
+
+import com.example.somina.myapp14.utils.CommonUtilities;
+
+public interface OnWebServiceResult {
+    public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
